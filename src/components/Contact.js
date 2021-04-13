d758c9265a079e3ddd800ee51317d0b9183e3be2
William <wvuong3@myseneca.ca>
2021-04-13 16:38:27 -0400

    fa icons fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,9 @@ const Contact = ({ contactData }) => {
         <>
             <li>
                 <a href={contactData.link} target="_blank">
-                    <i className={contactData.icon}></i>
+                    <div className="contactIcons">
+                        <i className={contactData.icon}></i>
+                    </div>
                 </a>
             </li>
         </>
@@ -13,27 +15,3 @@ const Contact = ({ contactData }) => {
 }
 
 export default Contact
-
-
-// {/* <ul id="contactLinks">
-//                     <li>
-//                         <a href="mailto:willvuong@hotmail.com">
-//                             <i className="fas fa-envelope-open-text fa-3x"></i>
-//                         </a>
-//                     </li>
-//                     <li>
-//                         <a href="https://www.linkedin.com/in/willlvuong" target="_blank"> {/* target="_blank" sends to new tab instead of hard link */}
-//                             <i className="fab fa-linkedin fa-3x"></i>
-//                         </a>
-//                     </li>
-//                     <li>
-//                         <a href="https://github.com/willvuong" target="_blank">
-//                             <i className="fab fa-github fa-3x"></i>
-//                         </a>
-//                     </li>
-//                     <li>
-//                         <a href="tel:6477085527">
-//                             <i className="fas fa-phone-square-alt fa-3x"></i>
-//                         </a>
-//                     </li>
-//                 </ul> */}
