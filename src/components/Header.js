753cfdda70491f6e0f9b45b59cc99f84ebcf6a19
William <wvuong3@myseneca.ca>
2021-04-14 16:00:20 -0400

    rearranged header components to make more logical sense

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,28 @@
+import { useState } from 'react'
+
 import Navigation from './Navigation.js'
+import MenuButton from './MenuButton.js'
+import NavigationResponsive from './NavigationResponsive.js'
 
 const Header = () => {
-    
+    const [showMenu, setShowMenu] = useState(false) //for hamburger menu (click hambuger to open, X to close)
+    //console.log({ showMenu })
+
     return (
         <header className='Header'>
             <div className="navbar">
                 <a href="/">
-                    <img src="mylogo.png" className="myLogo" alt="logo"/>
+                    <img src="mylogo.png" className="myLogo" alt="logo" />
                 </a>
                 <Navigation />
+                <MenuButton
+                    icon={showMenu ? <i class="fas fa-times fa-2x"></i> : <i class="fas fa-bars fa-2x"></i>} //X menu of true, hamburger if false
+                    onClick={() => setShowMenu(!showMenu)} //toggle between true and false for onClick result
+                />
             </div>
+
+            {showMenu && <NavigationResponsive />}
+
         </header>
     )
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,7 @@
-import { useState, useEffect } from 'react'
-import { Link, useLocation } from 'react-router-dom'
-
-
-import MenuButton from './MenuButton.js'
-import NavigationResponsive from './NavigationResponsive.js'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Navigation = () => {
-    const [showMenu, setShowMenu] = useState(false) //for hamburger menu (click hambuger to open, X to close)
-    //console.log({ showMenu })
-
     const [activeTab, setActiveTab] = useState('')
     // const currentLocation = window.location.href;
     // useEffect(() => {
@@ -26,15 +19,7 @@ const Navigation = () => {
                 <li className={activeTab === 'Projects' ? "navActive" : ""}><Link to='/projects' onClick={() => setActiveTab('Projects')}>Projects</Link></li>
             </ul>
             {/* {activeTab} */}
-            <MenuButton 
-            icon={showMenu ? <i class="fas fa-times fa-2x"></i> : <i class="fas fa-bars fa-2x"></i>} //X menu of true, hamburger if false
-            onClick={() => setShowMenu(!showMenu)} //toggle between true and false for onClick result
-            />
-            
-            {showMenu && <NavigationResponsive />}
         </nav>
-        
-
     )
 }
 
