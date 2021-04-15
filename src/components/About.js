0dfd799492d85581fc2ce55917c8eb77fde3e139
William <wvuong3@myseneca.ca>
2021-04-14 21:54:19 -0400

    about edit

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,6 @@ import AboutItems from './AboutItems.js'
 const About = ({ aboutData }) => {
     return (
         <div className="About">
-            <p>Hello, my name is William and I am a Computer Programming graduate from Seneca College.</p>
             <h2>What I Offer</h2>
             {aboutData.length > 0 && aboutData.map((data) => (
                 //console.log(data.title);
