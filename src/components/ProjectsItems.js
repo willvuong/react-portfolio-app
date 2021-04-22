adfdd4a7fbc3fb8db3d8e7ede38ca670124a5049
William <wvuong3@myseneca.ca>
2021-04-22 16:04:28 -0400

    project edit 3

--- src/components/ProjectsItems.js
+++ src/components/ProjectsItems.js
@@ -4,16 +4,21 @@ const ProjectsItems = ({ projectsData }) => {
         <div className="ProjectsItems">
             <div className="projectImgContainer"> {/* specifically for dark background on hover */}
                 <img src={projectsData.img} ></img>
+
+                <div className="projectInfo">
+                    <p>{projectsData.description}</p>
+                    <button>
+                        <a href={projectsData.demoLink} target="_blank">Demo</a>
+                    </button>
+                    <br />
+                    <button>
+                        <a href={projectsData.sourceCode} target="_blank">Source</a>
+                    </button>
+                </div>
             </div>
 
             <h3>{projectsData.name}</h3>
 
-            <div className="projectInfo">
-                <p>{projectsData.description}</p>
-                <a href={projectsData.demoLink} target="_blank">Demo</a>
-                <br />
-                <a href={projectsData.sourceCode} target="_blank">Source</a>
-            </div>
         </div>
     )
 }
--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -8,7 +8,7 @@ const projectsData = [
     {
         id: 1,
         name: "Hangman Game",
-        description: "",
+        description: "Hangman game that records correct and wrong letters, and determines on whether you can play depending on whether you win or lose, through the use of React useStates and useEffect. React useStates were used as props in order to update what was being displayed on the screen (ie: wrongLetters that were entered updated the Figure component to render out a new body part)",
         img: hangman, //insert image src
         demoLink: "", //insert webside url
         sourceCode: "https://github.com/willvuong/react-hangman-app" //insert heroku link
@@ -16,7 +16,7 @@ const projectsData = [
     {
         id: 2,
         name: "To-Do App",
-        description: "",
+        description: "A to-do list using React in order to add, delete and track task. Created functions that served as functionality in order to edit the task list that were passed into components as props. React useStates were incorporated in order to display/close the + button (add new task) and to add new tasks components into the task array.",
         img: todo, //insert image src
         demoLink: "", //insert webside url
         sourceCode: "https://github.com/willvuong/react-to-do-app" //insert heroku link
@@ -24,7 +24,7 @@ const projectsData = [
     {
         id: 3,
         name: "Movie Database App",
-        description: "",
+        description: "This movie database application pulls data from an API in order to display specfically queried movies from the user. A useState is intialized as null which it then stores the data of the query that the user enters in the search bar. All movies that match the specified keyword is firstly searched through the API -> converted to JSON objects -> results sent to movies component as props -> movie component where the data is then handled and rendered onto the page",
         img: moviedb, //insert image src
         demoLink: "", //insert webside url
         sourceCode: "" //insert heroku link
